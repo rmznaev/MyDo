f1c3de470d581eebac10b67deec37f838d112ec7
Ramazan Abdullayev <ramazan.aev@gmail.com>
2019-08-18 17:38:40 +0400

    Realm Dependency added to the project

---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '9.0'
+
+target 'MyDo' do
+
+  use_frameworks!
+
+  # Pods for MyDo
+
+    pod 'RealmSwift'
+
+end
---
+++ Pods/Target Support Files/Pods-MyDo/Pods-MyDo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyDo : NSObject
+@end
+@implementation PodsDummy_Pods_MyDo
+@end
